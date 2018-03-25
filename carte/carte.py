e393df9b9255735021cc2baa302a648ccf576f68
Unknown <killian.buhr@gmail.com>
2018-03-25 12:12:36 +0200

    update map file

--- carte/carte.py
+++
@@ -1,9 +0,0 @@
-import clize
-import folium
-import jinja2
-import selenium
-import xlrd
-
-xlsx='/home/formationsid/Bureau/Catastrophes_Naturelles/data/201511_Cartographie.xlsx'
-map_='/home/formationsid/Bureau/Catastrophes_Naturelles/render_map.py '
-python render_map.py  201511_Cartographie.xlsx
